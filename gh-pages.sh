084be57ee81306b715bc8f31b1ba44bdcfa83c7a
NicKAnderson038 <andersonnick038@gmail.com>
2020-05-24 10:50:39 -0400

    🚀 gh-pages deployment script

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,9 +1,28 @@
-git commit -am "Save uncommited changes (WIP)"
-git branch --delete --force gh-pages
-git checkout --orphan gh-pages
-git add -f dist
-git commit -m "Rebuild GitHub pages"
-git filter-branch -f --prune-empty --subdirectory-filter dist && git push -f origin gh-pages && git checkout -
+echo "Begin deployment ⚙️"
+
+# UPDATE with application name
+newPath='/custom-timepicker/'
+
+restoredPath='/'
+
+export PUBLIC_PATH=$newPath
+echo "GITHUB PAGES PATH 📰: $newPath"
+# node --print 'process.env.PUBLIC_PATH'
+
+# Build application
+yon build
+
+export PUBLIC_PATH=$restoredPath
+echo "RESTORED PATH 🔙: $restoredPath"
+# node --print 'process.env.PUBLIC_PATH'
+
+# (Skip if first deployment) rm gh-pages and redo steps above for changes
+git push origin --delete gh-pages
+
+git add -f dist && git commit -m "Initial dist subtree commit" --no-verify
+
+git subtree push --prefix dist origin gh-pages
 
 echo "Deployment complete 📦"
+exit 0
 read
