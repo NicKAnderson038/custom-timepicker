d792f13f8e88267547f03506caadb280e85ec99a
NicKAnderson038 <andersonnick038@gmail.com>
2020-05-24 11:35:04 -0400

    .

--- gh-pages.sh
+++ gh-pages.sh
@@ -18,15 +18,13 @@ echo "🚀 Begin deployment"
 ### (Skip if first deployment) rm gh-pages and redo steps above for changes
 git push origin --delete gh-pages
 
-# git rm -r --cached dist
-
 git add -f dist && git commit -m "Initial dist subtree commit" --no-verify
 
 git subtree push --prefix dist origin gh-pages
 
 rm -r -v dist
 
-git rm -r --cached
+git rm -r --cached dist
 
 git add .
 git commit -m "cleaned cache"
