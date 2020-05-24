e0687ed6ac59da8ab3e6232a4a266b506474f828
NicKAnderson038 <andersonnick038@gmail.com>
2020-05-24 11:15:32 -0400

    .

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,4 +1,4 @@
-echo "⚙️ Begin deployment"
+echo "⚙️  Begin deployment"
 
 # UPDATE with application name
 newPath='/custom-timepicker/'
@@ -19,7 +19,7 @@ git push origin --delete gh-pages
 
 git rm -r --cached dist
 
-git add -f dist && git commit -m "Initial dist subtree commit" --no-verify
+git add dist && git commit -m "Initial dist subtree commit" --no-verify
 
 git subtree push --prefix dist origin gh-pages
 
