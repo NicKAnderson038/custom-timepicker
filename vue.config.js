496e1155d438b1891a1499962018a2e34decac0e
NicKAnderson038 <andersonnick038@gmail.com>
2020-05-24 09:20:22 -0400

    ⚗

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   transpileDependencies: ['vuetify'],
   // publicPath: '/custom-timepicker/',
+  publicPath: process.env.PUBLIC_PATH ? '/custom-timepicker/' : '/',
 }
+
+console.log('TEST TEST TEST: ', process.env.PUBLIC_PATH)
