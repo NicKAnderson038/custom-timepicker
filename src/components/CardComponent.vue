308849f9a2942743a7944b30c05fc6e92e5e0b17
NicKAnderson038 <andersonnick038@gmail.com>
2020-05-24 08:53:20 -0400

    🐛 bug fix

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -8,20 +8,18 @@
       >
         <v-card-title>{{ title.toUpperCase() }}</v-card-title>
       </v-img>
-
       <v-card-subtitle class="pb-0">
         <v-icon class="caret--size">mdi-clock</v-icon>
         {{ subTitle }}
       </v-card-subtitle>
-
       <v-card-text class="text--primary">
         <ul v-for="feature in features" :key="feature.replace(/ /, '-')">
           <li>{{ feature }}</li>
         </ul>
       </v-card-text>
-
       <v-card-actions>
         <v-text-field
+          readonly
           :label="labels[0]"
           :color="color[0]"
           :value="timePicker1"
@@ -33,6 +31,7 @@
           :color="color[0]"
         />
         <v-text-field
+          readonly
           :label="labels[1]"
           :color="color[1]"
           :value="timePicker2"
@@ -46,23 +45,25 @@
       </v-card-actions>
       <v-card-actions>
         <v-text-field
+          readonly
           :label="labels[0]"
           :color="color[2]"
-          :value="timePicker1"
+          :value="timePicker3"
         ></v-text-field>
         <TimePicker
-          v-model="timePicker1"
+          v-model="timePicker3"
           :label="labels[0]"
           :time24hr="false"
           :color="color[2]"
         />
         <v-text-field
+          readonly
           :label="labels[1]"
           :color="color[3]"
-          :value="timePicker2"
+          :value="timePicker4"
         ></v-text-field>
         <TimePicker
-          v-model="timePicker2"
+          v-model="timePicker4"
           :label="labels[0]"
           :time24hr="false"
           :color="color[3]"
@@ -79,14 +80,15 @@ export default {
   components: {
     TimePicker,
   },
-
   data: () => ({
     title: 'Custom Time Picker',
     subTitle: 'Time Picker Features',
-    timePicker1: '',
     color: ['success', 'error', 'primary', 'warning'],
-    timePicker2: '',
     labels: ['Start Time', 'End Time'],
+    timePicker1: '',
+    timePicker2: '',
+    timePicker3: '',
+    timePicker4: '',
     features: [
       'Automatic time validation',
       '24hr and 12hr time formats',
